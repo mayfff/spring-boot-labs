4c4b3f3ef4e6c3a74210ee93a2665c15cd85c0cd
mayfff <zakrevsky.d.s@gmail.com>
2024-12-12 18:32:18 +0200

    prepared for lab2

---
+++ src/main/java/spring/lab/SpringBootLabsApplication.java
@@ -0,0 +1,12 @@
+package spring.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLabsApplication.class, args);
+	}
+}
