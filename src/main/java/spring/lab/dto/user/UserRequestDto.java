3cc38a3cec53d0233a022cef48d93f2235b3a051
mayfff <zakrevsky.d.s@gmail.com>
2024-12-24 23:01:28 +0200

    fixed validation mistake in UserRequestDto

--- src/main/java/spring/lab/dto/user/UserRequestDto.java
+++ src/main/java/spring/lab/dto/user/UserRequestDto.java
@@ -1,5 +1,6 @@
 package spring.lab.dto.user;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -11,10 +12,10 @@ public class UserRequestDto {
     @NotBlank(message = "Username is required")
     String username;
 
-    @NotBlank(message = "Email is required")
-    @Size(min = 8, message = "Password length must be more than 8")
+    @Email(message = "Email must be correct")
     String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password length must be more than 8")
     String password;
 }
